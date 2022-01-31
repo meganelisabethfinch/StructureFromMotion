f3923f31187aadf1bdd7b4e88dc7d3fa9f42e580
meganelisabethfinch <meganelisabethfinch@gmail.com>
2022-01-31 00:44:29 +0000

    Move Zhang RBA stuff

--- headers/cost/zhang_rba_error.h
+++ headers/cost/zhang_rba_error.h
@@ -8,6 +8,9 @@
 #include <array>
 #include <opencv2/core/types.hpp>
 #include <ceres/cost_function.h>
+#include <set>
+#include <headers/types.h>
+#include <headers/point_cloud.h>
 
 // Rotation-free cost function as described in
 // "Robust Bundle Adjustment for Structure from Motion" (Zhang et. al) (2006)
@@ -27,6 +30,11 @@ public:
             T* residuals) const;
 
     static ceres::CostFunction* Create(const std::array<std::array<cv::Point2d, 6>, 3>& points3d);
+
+    static std::set<double> GetPointsWithCommonViews(PointCloud &pointCloud, size_t N, size_t J);
+
+    static void CommonViewsHelper(PointCloud &pointCloud, size_t pointsNeeded, size_t viewsNeeded,
+         std::set<ImageID> &commonViews, std::vector<int> &points, std::set<double> &tuples);
 };
 
 #endif //SFM_ZHANG_RBA_ERROR_H

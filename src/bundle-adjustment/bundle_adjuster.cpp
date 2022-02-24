ad0166952344e6e29c6749e0dbe0fa23cf2b92b9
meganelisabethfinch <meganelisabethfinch@gmail.com>
2022-02-24 18:04:41 +0000

    Forgot to add a file

---
+++ src/bundle-adjustment/bundle_adjuster.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Megan Finch on 24/02/2022.
+//
+
+#include <headers/bundle-adjustment/bundle_adjuster.h>
+#include <headers/types.h>
+
+#include <headers/bundle-adjustment/dummy_bundle_adjuster.h>
+#include <headers/bundle-adjustment/basic_bundle_adjuster.h>
+#include <headers/bundle-adjustment/zhang_bundle_adjuster.h>
+
+cv::Ptr<BundleAdjuster> BundleAdjuster::create(const BundleAdjusterType& type) {
+    switch (type) {
+        case BundleAdjusterType::OFF:
+            return cv::makePtr<DummyBundleAdjuster>();
+        case BundleAdjusterType::BASIC:
+            return cv::makePtr<BasicBundleAdjuster>();
+        case BundleAdjusterType::ZHANG:
+            return cv::makePtr<ZhangBundleAdjuster>();
+    }
+}

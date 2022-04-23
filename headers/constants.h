67a1df41106b291379daa0ac5520a90c33a08f9c
meganelisabethfinch <meganelisabethfinch@gmail.com>
2022-04-23 19:01:40 +0100

    Trying parameters from March 30

--- headers/constants.h
+++ headers/constants.h
@@ -10,13 +10,13 @@ const double LOWE_RATIO = 0.6;
 const double FOCAL_LENGTH = 2500;
 
 // Reject a 3d point if its re-projection error is above this threshold
-const double REPROJECTION_ERROR_THRESHOLD = 10.0;
+const double REPROJECTION_ERROR_THRESHOLD = 3.0;
 
 // RANSAC inlier threshold
 const double RANSAC_THRESHOLD = 10.0;
 
 // Reject a camera pose if the ratio of inliers is below this threshold
-const double POSE_INLIERS_MINIMAL_RATIO = 0.25;
+const double POSE_INLIERS_MINIMAL_RATIO = 0.5;
 
 // Number of matches needed for an edge between two images
 const int SCENE_GRAPH_EDGE_THRESHOLD = 25;
@@ -24,14 +24,14 @@ const int SCENE_GRAPH_EDGE_THRESHOLD = 25;
 const double MERGE_CLOUD_POINT_MIN_MATCH_DISTANCE = 0.01;
 const double MERGE_CLOUD_FEATURE_MIN_MATCH_DISTANCE = 20.0;
 
-const int PNP_RANSAC_MAX_ITERATIONS = 1000;
-const double PNP_RANSAC_CONFIDENCE = 0.999;
+const int PNP_RANSAC_MAX_ITERATIONS = 100;
+const double PNP_RANSAC_CONFIDENCE = 0.99;
 
-const double FM_RANSAC_THRESHOLD = 10.0; // 3.0
-const double FM_RANSAC_CONFIDENCE = 0.999; // 0.99
+const double FM_RANSAC_THRESHOLD = 3.0; // 3.0
+const double FM_RANSAC_CONFIDENCE = 0.99; // 0.99
 
 // Termination criteria for bundle adjustment
-const double BA_ETA = 1e-1;
+const double BA_ETA = 1e-2;
 
 // Number of matching points needed to calculate matrices
 const int POINTS_NEEDED_HOMOGRAPHY_MATRIX = 100;
